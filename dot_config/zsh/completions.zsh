a65c429b05c9396948097986f3aba0d8b349a52d
stengah <deepsghimire1@gmail.com>
2022-05-01 17:50:51 +0545

    Update dot_config/zsh/completions.zsh

--- dot_config/zsh/completions.zsh
+++ dot_config/zsh/completions.zsh
@@ -1,14 +1,15 @@
 
+
 autoload -Uz compinit
 compinit
 zstyle ':completion:*' use-cache on
 zstyle ':completion:*' cache-path ~/.zsh/cache
 zstyle ':completion:*' completer _complete _match _approximate
 zstyle ':completion:*:match:*' original only
+zstyle ':completion:*:approximate:*' max-errors 1 numeric
 zstyle -e ':completion:*:approximate:*' \
+        max-errors 'reply=($((($#PREFIX+$#SUFFIX)/3))numeric)'
 zstyle ':completion:*:functions' ignored-patterns '_*'
-zstyle ':completion:*:*:xdvi:*' menu yes select
-zstyle ':completion:*:*:xdvi:*' file-sort time
 zstyle ':completion:*:*:kill:*' menu yes select
 zstyle ':completion:*:kill:*'   force-list alway
 zstyle ':completion:*' squeeze-slashes true
