b75a1393ea8cb569324e0f6b87a68233f01f608d
stengah <deepsghimire1@gmail.com>
2022-05-30 21:19:49 +0545

    Update dot_config/zsh/init.zsh

--- dot_config/zsh/init.zsh
+++ dot_config/zsh/init.zsh
@@ -15,3 +15,20 @@ bindkey '' run-help
 
 
 bindkey -v
+
+function emacs {
+    if [[ $# -eq 0 ]]; then
+        /usr/bin/emacs # "emacs" is function, will cause recursion
+        return
+    fi
+    args=($*)
+    for ((i=0; i <= ${#args}; i++)); do
+        local a=${args[i]}
+        # NOTE: -c for creating new frame
+        if [[ ${a:0:1} == '-' && ${a} != '-c' && ${a} != '--' ]]; then
+            /usr/bin/emacs ${args[*]}
+            return
+        fi
+    done
+    setsid emacsclient -n -a /usr/bin/emacs ${args[*]}
+}
